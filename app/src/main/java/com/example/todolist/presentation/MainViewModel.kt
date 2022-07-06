1fba828857c2e28826f9d8b9c46c9dda8974a756
alexsmik <alexsmik@yandex.ru>
2022-07-06 10:19:44 +0300

    add MainViewModel

--- app/src/main/java/com/example/todolist/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/todolist/presentation/MainViewModel.kt
@@ -1,15 +1,34 @@
 package com.example.todolist.presentation
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.todolist.data.ShopListRepositoryImpl
 import com.example.todolist.domain.GetShopListUseCase
 import com.example.todolist.domain.DeleteShopItemUseCase
 import com.example.todolist.domain.EditShopItemUseCase
+import com.example.todolist.domain.ShopItem
 
 class MainViewModel : ViewModel() {
+
     private val repository = ShopListRepositoryImpl
+
     private val getShopListUseCase = GetShopListUseCase(repository)
     private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
     private val editShopItemUseCase = EditShopItemUseCase(repository)
 
+    val shopList = MutableLiveData<List<ShopItem>>()
+
+    fun getShopList() {
+        val list = getShopListUseCase.getShopList()
+        shopList.value = list
+    }
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+        getShopList()
+    }
+    fun changeEnableState(shopItem: ShopItem) {
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newItem)
+        getShopList()
+    }
 }
