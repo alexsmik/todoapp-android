088ecb71ea251fbf2782700f28f569de0c59fe7f
alexsmik <alexsmik@yandex.ru>
2022-07-07 12:56:32 +0300

    setup LinearLayout

--- app/src/main/java/com/alexsmik/todolist/presentation/MainActivity.kt
+++ app/src/main/java/com/alexsmik/todolist/presentation/MainActivity.kt
@@ -3,24 +3,35 @@ package com.alexsmik.todolist.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.LinearLayout
 import androidx.lifecycle.ViewModelProvider
 import com.alexsmik.todolist.R
+import com.alexsmik.todolist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
-    private var count = 0
+    private lateinit var llShopList: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        llShopList = findViewById(R.id.ll_shop_list)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel.shopList.observe(this) {
-            Log.d("MainActivityTest", it.toString())
-            if (count == 0) {
-                count++
-                val item = it[0]
-                viewModel.deleteShopItem(item)
+            showList(it)
+        }
+    }
+    private fun showList(list: List<ShopItem>) {
+        for (shopItem in list) {
+            val layoutId = if (shopItem.enabled) {
+                R.layout.item_shop_enabled
+            } else {
+                R.layout.item_shop_disabled
             }
+            val view = LayoutInflater.from(this).inflate(layoutId, llShopList, false)
+            llShopList.addView(view)
         }
     }
 }
