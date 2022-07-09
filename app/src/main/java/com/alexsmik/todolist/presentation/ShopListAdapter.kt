89b6caf65429b2d6ef1aabc310d635828411097b
alexsmik <alexsmik@yandex.ru>
2022-07-09 13:26:27 +0300

    setup adapter

--- app/src/main/java/com/alexsmik/todolist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/alexsmik/todolist/presentation/ShopListAdapter.kt
@@ -53,6 +53,7 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
     companion object {
         const val VIEW_TYPE_ENABLED = 100
         const val VIEW_TYPE_DISABLED = 101
+        const val MAX_POOL_SIZE = 10
     }
 
 }
