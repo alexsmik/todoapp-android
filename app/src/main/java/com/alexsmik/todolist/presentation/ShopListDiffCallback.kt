dcbcea59823ba9c21487ce5c135d528615ff2448
alexsmik <alexsmik@yandex.ru>
2022-07-09 17:48:19 +0300

    add DiffUtil

---
+++ app/src/main/java/com/alexsmik/todolist/presentation/ShopListDiffCallback.kt
@@ -0,0 +1,25 @@
+package com.alexsmik.todolist.presentation
+
+import androidx.recyclerview.widget.DiffUtil
+import com.alexsmik.todolist.domain.ShopItem
+
+class ShopListDiffCallback (
+    private val oldList: List<ShopItem>,
+    private val newList: List<ShopItem>,
+): DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getNewListSize(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+}
