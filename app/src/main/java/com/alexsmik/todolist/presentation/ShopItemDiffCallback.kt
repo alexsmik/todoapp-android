8e5104fa2f56ba105c31f1b1e3a9d60e85301d10
alexsmik <alexsmik@yandex.ru>
2022-07-12 13:08:03 +0300

    add DiffCallback

--- app/src/main/java/com/alexsmik/todolist/presentation/ShopItemDiffCallback.kt
+++ app/src/main/java/com/alexsmik/todolist/presentation/ShopItemDiffCallback.kt
@@ -5,10 +5,10 @@ import com.alexsmik.todolist.domain.ShopItem
 
 class ShopItemDiffCallback: DiffUtil.ItemCallback<ShopItem>() {
     override fun areItemsTheSame(oldItem: ShopItem, newItem: ShopItem): Boolean {
-        TODO("Not yet implemented")
+        return oldItem.id == newItem.id
     }
 
     override fun areContentsTheSame(oldItem: ShopItem, newItem: ShopItem): Boolean {
-        TODO("Not yet implemented")
+        return oldItem == newItem
     }
 }
