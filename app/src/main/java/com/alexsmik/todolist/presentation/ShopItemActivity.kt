c748200d7ca0cf3ba66cb1ecf8afa200b4337606
alexsmik <alexsmik@yandex.ru>
2022-07-15 15:44:11 +0300

    setup ItemActivity

---
+++ app/src/main/java/com/alexsmik/todolist/presentation/ShopItemActivity.kt
@@ -0,0 +1,15 @@
+package com.alexsmik.todolist.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.alexsmik.todolist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    private lateinit var viewModel: ShopItemViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+        viewModel.errorInputName.value = false
+    }
+}
