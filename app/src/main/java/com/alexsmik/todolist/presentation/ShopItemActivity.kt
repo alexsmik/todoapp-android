dfa14572f8303f856f7b18e1db62d8951b0a8658
alexsmik <alexsmik@yandex.ru>
2022-07-16 13:38:13 +0300

    add newIntent

--- app/src/main/java/com/alexsmik/todolist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/alexsmik/todolist/presentation/ShopItemActivity.kt
@@ -1,5 +1,7 @@
 package com.alexsmik.todolist.presentation
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -14,10 +16,15 @@ class ShopItemActivity : AppCompatActivity() {
         Log.d("ShopItemActivity", mode.toString())
     }
     companion object {
-        const val EXTRA_SCREEN_MODE = "extra_mode"
-        const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
-        const val MODE_EDIT = "mode_edit"
-        const val MODE_ADD = "mode_add"
+        private const val EXTRA_SCREEN_MODE = "extra_mode"
+        private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
+        private const val MODE_EDIT = "mode_edit"
+        private const val MODE_ADD = "mode_add"
+    }
+    fun newIntentAddItem(context: Context): Intent {
+        val intent = Intent(context, ShopItemActivity::class.java)
+        intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
+        return intent
     }
 
 }
