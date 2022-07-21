95feda89bf0f2735930350734b2970587ca8056e
alexsmik <alexsmik@yandex.ru>
2022-07-21 17:26:42 +0300

    add coroutines

--- app/src/main/java/com/alexsmik/todolist/domain/GetShopItemUseCase.kt
+++ app/src/main/java/com/alexsmik/todolist/domain/GetShopItemUseCase.kt
@@ -2,7 +2,7 @@ package com.alexsmik.todolist.domain
 
 class GetShopItemUseCase(private val shopListRepository: ShopListRepository) {
 
-    fun getShopItem(shopItemId: Int): ShopItem {
+    suspend fun getShopItem(shopItemId: Int): ShopItem {
         return shopListRepository.getShopItem(shopItemId)
     }
 }
