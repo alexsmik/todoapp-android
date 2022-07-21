3b67e5d99d6b5fca40ccd01c1ff44029c5b3e953
alexsmik <alexsmik@yandex.ru>
2022-07-21 17:27:10 +0300

    add coroutines

--- app/src/main/java/com/alexsmik/todolist/presentation/MainViewModel.kt
+++ app/src/main/java/com/alexsmik/todolist/presentation/MainViewModel.kt
@@ -1,15 +1,18 @@
 package com.alexsmik.todolist.presentation
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
 import com.alexsmik.todolist.data.ShopListRepositoryImpl
 import com.alexsmik.todolist.domain.DeleteShopItemUseCase
 import com.alexsmik.todolist.domain.EditShopItemUseCase
 import com.alexsmik.todolist.domain.GetShopListUseCase
 import com.alexsmik.todolist.domain.ShopItem
+import kotlinx.coroutines.launch
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val repository = ShopListRepositoryImpl
+    private val repository = ShopListRepositoryImpl(application)
 
     private val getShopListUseCase = GetShopListUseCase(repository)
     private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
@@ -18,11 +21,15 @@ class MainViewModel : ViewModel() {
     val shopList = getShopListUseCase.getShopList()
 
     fun deleteShopItem(shopItem: ShopItem) {
-        deleteShopItemUseCase.deleteShopItem(shopItem)
+        viewModelScope.launch {
+            deleteShopItemUseCase.deleteShopItem(shopItem)
+        }
     }
 
     fun changeEnableState(shopItem: ShopItem) {
-        val newItem = shopItem.copy(enabled = !shopItem.enabled)
-        editShopItemUseCase.editShopItem(newItem)
+        viewModelScope.launch {
+            val newItem = shopItem.copy(enabled = !shopItem.enabled)
+            editShopItemUseCase.editShopItem(newItem)
+        }
     }
 }
